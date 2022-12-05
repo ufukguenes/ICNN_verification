752046d106fdae53181583181ed70285847cab4a
ufukguenes <u.guenes@live.de>
2022-12-05 02:20:28 +0100

    .

--- script/main.py
+++ script/main.py
@@ -1,53 +1,74 @@
+import numpy as np
 import torch
 
-from script.Networks import ICNN, SequentialNN
+from script.Networks import ICNN
+from script.Networks import SequentialNN
 from torch.utils.data import DataLoader
 from script.dataInit import ConvexDataset, Rhombus
-from script.trainFunction import train_icnn
+from script.trainFunction import train_icnn_adversarial, train_icnn
 from script.eval import Plots_for
 from script.Verification import verification
 
-sequential = False
+icnn = ICNN([2, 10, 10, 1])
+adversarial = SequentialNN([2, 10, 10, 2])
 epochs = 1
-batch_size = 1
+batch_size = 10
+
+number_of_adversarial_samples = 10000
 number_of_train_samples = 10000
 hyper_lambda = 1
 x_range = [-1.5, 1.5]
 y_range = [-1.5, 1.5]
 
-if not sequential:
-    icnn = ICNN([2, 10, 10, 1])
+init = False
+
+true_extremal_points = Rhombus().get_extremal_points()
+if init:
+    included_space, ambient_space = Rhombus().get_uniform_samples(number_of_train_samples, x_range, y_range) #samples will be split in inside and outside the rhombus
+    dataset = ConvexDataset(data=included_space)
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    dataset = ConvexDataset(data= ambient_space)
+    ambient_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    random_samples = []
+    for i in range(number_of_adversarial_samples): # generate random samples
+        x = np.random.default_rng().uniform(low=x_range[0], high=x_range[1])
+        y = np.random.default_rng().uniform(low=y_range[0], high=y_range[1])
+        random_samples.append([[x, y]])
+
+    value = torch.tensor(random_samples, requires_grad=True, dtype=torch.float32)
+    adversarial_set = ConvexDataset(data=value)
+    adversarial_loader = DataLoader(adversarial_set, batch_size=batch_size, shuffle=True)
+
+if init:
+    torch.save(ambient_space, "ambient_space.pt")
+    torch.save(included_space, "included_space.pt")
+    torch.save(value, "value.pt")
 else:
-    icnn = SequentialNN([2, 10, 10, 1])
+    icnn.load_state_dict(torch.load("icnn.pt"), strict=False)
+    ambient_space = torch.load("ambient_space.pt")
+    included_space = torch.load("included_space.pt")
+    value = torch.load("value.pt")
+    dataset = ConvexDataset(data=included_space)
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    dataset = ConvexDataset(data=ambient_space)
+    ambient_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    adversarial_set = ConvexDataset(data=value)
+    adversarial_loader = DataLoader(adversarial_set, batch_size=batch_size, shuffle=True)
 
-included_space, ambient_space = Rhombus().get_uniform_samples(number_of_train_samples, x_range,
-                                                              y_range)  # samples will be split in inside and outside the rhombus
-true_extremal_points = Rhombus().get_extremal_points()
-dataset = ConvexDataset(data=included_space)
-train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-dataset = ConvexDataset(data=ambient_space)
-ambient_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-#torch.save(ambient_space, "ambient_space.pt")
-#torch.save(included_space, "included_space.pt")
-
-plots = Plots_for(0, icnn, included_space, ambient_space, true_extremal_points, x_range, y_range)
+plots = Plots_for(0, icnn, included_space, ambient_space, true_extremal_points, x_range, y_range, adversarial=adversarial, adversarial_values=value)
 plots.plt_initial()
 
-history = train_icnn(icnn, train_loader, ambient_loader, epochs=epochs, sequential=sequential, hyper_lambda=0.1)
+plots.plt_adversarial_initial()
+plots.plt_adversarial_dotted()
+
+if init:
+    history = train_icnn(icnn, train_loader, ambient_loader, epochs=epochs, hyper_lambda=hyper_lambda)
+    torch.save(icnn.state_dict(), "icnn.pt")
 plots.plt_mesh()
-#torch.save(icnn.state_dict(), "icnn.pt")
 
-"""icnn.load_state_dict(torch.load("icnn.pt"), strict=False)
-ambient_space = torch.load("ambient_space.pt")
-included_space = torch.load("included_space.pt")
-true_extremal_points = Rhombus().get_extremal_points()
-
-plots = Plots_for(0, icnn, included_space, ambient_space, true_extremal_points, x_range, y_range)
-plots.plt_initial()
-plots.plt_mesh()"""
-
-result = verification(icnn, sequential)
-
-plots.c = result
-plots.plt_mesh()
+for i in range(100):
+    history = train_icnn_adversarial(icnn, adversarial, train_loader, adversarial_loader, epochs=epochs, train_ICNN=False, hyper_lambda=hyper_lambda, use_max_distance=True)
+    if i % 10 == 0:
+        print("run {}".format(i))
+        plots.plt_adversarial_dotted()
