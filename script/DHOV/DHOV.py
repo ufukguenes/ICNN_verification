simplified architekture and fixed bug in normalization

script/DHOV/DHOV.py
@@ -120,9 +120,6 @@ def start_verification(nn: SequentialNN, input, eps=0.001, solver_time_limit=Non
         ws = list(current_icnn.ws.parameters())  # bias for first relu activation with weights from us (in second layer)
         us = list(current_icnn.us.parameters())
 
-        plots = Plots_for(0, current_icnn, normalized_included_space.detach(), normalized_ambient_space.detach(), [-3, 3], [-3, 3])
-        plots.plt_mesh()
-
         # train icnn
         untouched_normalized_ambient_space = normalized_ambient_space.detach().clone()
         if use_grad_descent:
@@ -164,10 +161,6 @@ def start_verification(nn: SequentialNN, input, eps=0.001, solver_time_limit=Non
         else:
             train_icnn(current_icnn, train_loader, ambient_loader, epochs=icnn_epochs, hyper_lambda=1, optimizer=optimizer)
 
-        plots = Plots_for(0, current_icnn, normalized_included_space.detach(), normalized_ambient_space.detach(),
-                          [-3, 3], [-3, 3])
-        plots.plt_mesh()
-
         if train_outer:
             lam = 10
 
@@ -199,9 +192,7 @@ def start_verification(nn: SequentialNN, input, eps=0.001, solver_time_limit=Non
             plots = Plots_for(0, current_icnn, included_space.detach(), ambient_space.detach(), [-1, 3], [-1, 3])
             plots.plt_dotted()
 
-        plots = Plots_for(0, current_icnn, included_space.detach(), ambient_space.detach(),
-                          [-3, 3], [-3, 3])
-        plots.plt_mesh()
+
         # verify and enlarge convex approximation
         if use_over_approximation:
             if i == 0:
@@ -227,8 +218,7 @@ def start_verification(nn: SequentialNN, input, eps=0.001, solver_time_limit=Non
         with torch.no_grad():
             last_layer = list(current_icnn.ws[-1].parameters())
             b = last_layer[1]
-            b.data = b + c
-        plots.plt_mesh()
+            b.data = b - c
 
         c_values.append(c)
 
@@ -315,16 +305,16 @@ def init_icnn_box_bounds_with_softmax(icnn: ICNN_Softmax, box_bounds):
             us = list(icnn.us[i].parameters())
             us[0].data = torch.zeros_like(us[0], dtype=torch.float64)"""
 
-        us = list(icnn.us[-1].parameters())  # us is used because values in ws are set to 0 when negative
-        u = torch.zeros_like(us[0], dtype=torch.float64)
-        b = torch.zeros_like(us[1], dtype=torch.float64)
+        bb = list(icnn.ls[0].parameters())  # us is used because values in ws are set to 0 when negative
+        u = torch.zeros_like(bb[0], dtype=torch.float64)
+        b = torch.zeros_like(bb[1], dtype=torch.float64)
         for i in range(len(box_bounds)):
             u[2 * i][i] = 1
             u[2 * i + 1][i] = -1
             b[2 * i] = - box_bounds[1][i]  # upper bound
             b[2 * i + 1] = box_bounds[0][i]  # lower bound
-        us[0].data = u
-        us[1].data = b
+        bb[0].data = u
+        bb[1].data = b
 
 
 

script/DHOV/MainDHOV.py
@@ -160,10 +160,10 @@ def net_2d():
 
     test_image = torch.tensor([[0, 0]], dtype=torch.float64)
     icnns, c_values = \
-        dhov.start_verification(nn, test_image, eps=1, icnn_epochs=100, sample_new=True, use_over_approximation=True,
+        dhov.start_verification(nn, test_image, eps=1, icnn_epochs=500, sample_new=True, use_over_approximation=True,
                                 sample_over_input_space=False, sample_over_output_space=True,
                                 keep_ambient_space=False, use_grad_descent=False, train_outer=False,
-                                should_plot="none", optimizer="adam")
+                                should_plot="detailed", optimizer="adam")
 
 def cifar_net():
     batch_size = 10

script/DHOV/Normalisation.py
@@ -21,7 +21,6 @@ def normalize_nn(nn, mean, std, isICNN=False):
             parameter_list[0].data = torch.div(parameter_list[0], std)
             parameter_list[1].data = torch.add(- torch.matmul(parameter_list[0], mean), parameter_list[1])
 
-
     else:
         with torch.no_grad():
             parameter_list = list(nn.ws[0].parameters())
@@ -30,7 +29,7 @@ def normalize_nn(nn, mean, std, isICNN=False):
 
             k = len(nn.us)
             l = len(nn.ws)
-            for i in range(len(nn.us) - 1):
+            for i in range(len(nn.us)):
                 parameter_list = list(nn.us[i].parameters())
                 parameter_list[0].data = torch.div(parameter_list[0], std)
 
@@ -38,7 +37,7 @@ def normalize_nn(nn, mean, std, isICNN=False):
                 internal_parameter_list[1].data = torch.add(- torch.matmul(parameter_list[0], mean),
                                                             internal_parameter_list[1])
 
-            parameter_list = list(nn.us[-1].parameters())
+            parameter_list = list(nn.ls[0].parameters())
             parameter_list[0].data = torch.div(parameter_list[0], std)
             parameter_list[1].data = torch.add(- torch.matmul(parameter_list[0], mean), parameter_list[1])
 

script/NeuralNets/Networks.py
@@ -88,7 +88,7 @@ class ICNN_Softmax(nn.Module):
 
         d_in = layer_widths[1]
 
-        for i, lw in enumerate(layer_widths[2:]):
+        for lw in layer_widths[2:]:
             w = nn.Linear(d_in, lw, dtype=torch.float64)
 
             with torch.no_grad():
@@ -102,39 +102,34 @@ class ICNN_Softmax(nn.Module):
 
             self.ws.append(w)
             self.us.append(u)
-        self.us.append(nn.Linear(layer_widths[0], 2 * layer_widths[0], bias=True, dtype=torch.float64))
 
+        self.ls.append(nn.Linear(layer_widths[0], 2 * layer_widths[0], bias=True, dtype=torch.float64))
         self.ls.append(nn.Linear(layer_widths[0], 2 * layer_widths[0], bias=False, dtype=torch.float64))
         self.ls.append(nn.Linear(2 * layer_widths[0], 2 * layer_widths[0] - 1, bias=False, dtype=torch.float64))
 
         with torch.no_grad():
-            l1 = list(self.ls[0].parameters())
-            l2 = list(self.ls[1].parameters())
+            l1 = list(self.ls[1].parameters())
+            l2 = list(self.ls[2].parameters())
             l1[0].data = torch.tensor([[1, 1], [1, -1], [-1, 1], [-1, -1]], dtype=torch.float64)
             l2[0].data = torch.tensor([[1, 0, 0, -1], [0, 1, 0, -1], [0, 0, 1, -1]], dtype=torch.float64)
 
     def forward(self, x):
         x = Flatten()(x)
         x1 = nn.ReLU()(self.ws[0](x))  # first layer is only W
-        len_ws = len(self.ws)
-        for w, u in zip(self.ws[1: len_ws - 2], self.us[:-1]):
+        for w, u in zip(self.ws[1:-1], self.us[:-1]):
             a = w(x1)
             b = u(x)
             x1 = nn.ReLU()(a + b)
 
-        # something like and
-        icnn_out = self.ws[len_ws - 2](x1)
-        icnn_out = torch.nn.ReLU()(icnn_out)
-        w1 = self.ws[-1]
-        u1 = self.us[len(self.us) - 2]
-        icnn_out = w1(icnn_out) + u1(x)
+        icnn_out = self.ws[-1](x1) + self.us[-1](x)  # no ReLU in last layer"""
 
-        box_out = self.us[-1](x)
+        # something like and
+        box_out = self.ls[0](x)
         box_out = torch.max(box_out, dim=1, keepdim=True)[0]
         x_in = torch.cat([icnn_out, box_out], dim=1)
 
-        x_in = self.ls[0](x_in)
         x_in = self.ls[1](x_in)
+        x_in = self.ls[2](x_in)
         out = torch.max(x_in, dim=1)[0]
 
         return out