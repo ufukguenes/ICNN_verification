fixing bug in milp and sn verification caused by wrong calculation and use of upper and lower bounds

script/DHOV/MainDHOV.py
@@ -199,10 +199,10 @@ def net_2d():
 
     input_flattened = torch.flatten(test_image)
     input_size = input_flattened.size(0)
-    bounds = nn.calculate_box_bounds([input_flattened.add(-1), input_flattened.add(1)], with_relu=True)
+    bounds_affine_out, bounds_layer_out = nn.calculate_box_bounds([input_flattened.add(-1), input_flattened.add(1)])
 
     test_space = torch.empty((0, input_flattened.size(0)), dtype=data_type).to(device)
-    box_bound_output_space = ds.samples_uniform_over(test_space, 1000, bounds[-1])
+    box_bound_output_space = ds.samples_uniform_over(test_space, 1000, bounds_layer_out[-1])
 
     in_snv = []
     in_milp = []
@@ -323,10 +323,10 @@ def multi_net2D():
 
     input_flattened = torch.flatten(test_image)
     input_size = input_flattened.size(0)
-    bounds = nn.calculate_box_bounds([input_flattened.add(-1), input_flattened.add(1)], with_relu=True)
+    bounds_affine_out, bounds_layer_out = nn.calculate_box_bounds([input_flattened.add(-1), input_flattened.add(1)])
 
     test_space = torch.empty((0, input_flattened.size(0)), dtype=data_type).to(device)
-    box_bound_output_space = ds.samples_uniform_over(test_space, 100, bounds[-1])
+    box_bound_output_space = ds.samples_uniform_over(test_space, 100, bounds_layer_out[-1])
 
     in_dhov = []
     #in_dhov_affine = []

script/DHOV/MultiDHOV.py
@@ -55,8 +55,7 @@ def start_verification(nn: SequentialNN, input, icnns, group_size, eps=0.001, ic
 
     input_flattened = torch.flatten(input)
     eps_bounds = [input_flattened.add(-eps), input_flattened.add(eps)]
-    box_bounds = nn.calculate_box_bounds(
-        eps_bounds, with_relu=True)  # todo abbrechen, wenn die box bounds schon die eigenschaft erfüllen
+    bounds_affine_out, bounds_layer_out = nn.calculate_box_bounds(eps_bounds)  # todo abbrechen, wenn die box bounds schon die eigenschaft erfüllen
 
     included_space = torch.empty((0, input_flattened.size(0)), dtype=data_type).to(device)
     included_space = ds.samples_uniform_over(included_space, int(sample_count / 2), eps_bounds)
@@ -88,7 +87,7 @@ def start_verification(nn: SequentialNN, input, icnns, group_size, eps=0.001, ic
             else:
                 # todo test for when lower/upper bound is smaller then eps
                 ambient_space = ds.sample_uniform_excluding(ambient_space, int(sample_count / 2),
-                                                            box_bounds[current_layer_index - 1],
+                                                            bounds_affine_out[current_layer_index - 1],
                                                             icnn=icnns[current_layer_index - 1],
                                                             padding=0.5)
 
@@ -119,10 +118,10 @@ def start_verification(nn: SequentialNN, input, icnns, group_size, eps=0.001, ic
 
         if sample_over_output_space:
             ambient_space = ds.samples_uniform_over(ambient_space, int(sample_count / 2),
-                                                    box_bounds[current_layer_index], padding=0.5)
+                                                    bounds_layer_out[current_layer_index], padding=0.5)
             if should_plot in ["simple", "detailed"]:
                 original_ambient_space = ds.samples_uniform_over(original_ambient_space, int(sample_count / 2),
-                                                                 box_bounds[current_layer_index], padding=0.5)
+                                                                 bounds_layer_out[current_layer_index], padding=0.5)
 
         if should_plot == "detailed":
             plt_inc_amb("enhanced ambient space " + str(i), included_space.tolist(), ambient_space.tolist())
@@ -155,8 +154,8 @@ def start_verification(nn: SequentialNN, input, icnns, group_size, eps=0.001, ic
             ambient_loader = DataLoader(dataset, batch_size=icnn_batch_size)
 
             if init_network:
-                low = box_bounds[current_layer_index][0][from_to_neurons[0]: from_to_neurons[1]]
-                up = box_bounds[current_layer_index][1][from_to_neurons[0]: from_to_neurons[1]]
+                low = bounds_layer_out[current_layer_index][0][from_to_neurons[0]: from_to_neurons[1]]
+                up = bounds_layer_out[current_layer_index][1][from_to_neurons[0]: from_to_neurons[1]]
                 low = torch.div(torch.add(low, -mean), std)
                 up = torch.div(torch.add(up, -mean), std)
                 current_icnn.init(low, up)
@@ -256,7 +255,7 @@ def start_verification(nn: SequentialNN, input, icnns, group_size, eps=0.001, ic
 
         if sample_new:
             # todo entweder über box bounds sampeln oder über maximum radius
-            included_space, ambient_space = ds.sample_max_radius(icnns[current_layer_index], sample_count)
+            included_space, ambient_space = ds.sample_max_radius(icnns[current_layer_index], sample_count, bounds_affine_out, bounds_layer_out)
 
         else:
             included_space, ambient_space = ds.regroup_samples(icnns[current_layer_index], included_space, ambient_space, group_size)

script/NeuralNets/Networks.py
@@ -19,15 +19,15 @@ class VerifiableNet(ABC):
         self.ws = None
 
     @abstractmethod
-    def calculate_box_bounds(self, input_bounds, with_relu=True):
+    def calculate_box_bounds(self, input_bounds):
         pass
 
     @abstractmethod
-    def add_constraints(self, model, input_vars, bounds):
+    def add_constraints(self, model, input_vars, bounds_affine_out, bounds_layer_out):
         pass
 
-    def add_max_output_constraints(self, model, input_vars, bounds):
-        output = self.add_constraints(model, input_vars, bounds)
+    def add_max_output_constraints(self, model, input_vars, bounds_affine_out, bounds_layer_out):
+        output = self.add_constraints(model, input_vars, bounds_affine_out, bounds_layer_out)
         model.addConstr(output[0] <= 0)
         return output
 
@@ -62,7 +62,7 @@ class SequentialNN(nn.Sequential, VerifiableNet):
         x = Flatten()(x)
         return super().forward(x)
 
-    def calculate_box_bounds(self, input_bounds, with_relu=True):
+    def calculate_box_bounds(self, input_bounds):
         parameter_list = list(self.parameters())
 
         if input_bounds is None:
@@ -71,44 +71,51 @@ class SequentialNN(nn.Sequential, VerifiableNet):
                                 range(0, len(parameter_list), 2)]
             return bounds_per_layer  # todo None entfernen aus aufrufen und durch sinnvolle eingabe ersetzen
 
-        next_lower_bounds = input_bounds[0]
-        next_upper_bounds = input_bounds[1]
-        bounds_per_layer = []
+        affine_in_lb = input_bounds[0]
+        affine_in_ub = input_bounds[1]
+        affine_out_bounds_per_layer = []
+        layer_out_bounds_per_layer = []
         for i in range(0, len(parameter_list), 2):
             W, b = parameter_list[i], parameter_list[i + 1]
             w_plus = torch.maximum(W, torch.tensor(0, dtype=data_type).to(device))
             w_minus = torch.minimum(W, torch.tensor(0, dtype=data_type).to(device))
-            lb = torch.matmul(w_plus, next_lower_bounds).add(torch.matmul(w_minus, next_upper_bounds)).add(b)
-            ub = torch.matmul(w_plus, next_upper_bounds).add(torch.matmul(w_minus, next_lower_bounds)).add(b)
+            relu_in_lb = torch.matmul(w_plus, affine_in_lb).add(torch.matmul(w_minus, affine_in_ub)).add(b)
+            relu_in_ub = torch.matmul(w_plus, affine_in_ub).add(torch.matmul(w_minus, affine_in_lb)).add(b)
 
-            if with_relu and i < len(parameter_list) - 2:
-                next_upper_bounds = torch.maximum(torch.tensor(0, dtype=data_type).to(device), ub)
-                next_lower_bounds = torch.maximum(torch.tensor(0, dtype=data_type).to(device), lb)
+            affine_out_bounds_per_layer.append([relu_in_lb, relu_in_ub])
+
+            if i < len(parameter_list) - 2:
+                relu_out_lb = torch.maximum(torch.tensor(0, dtype=data_type).to(device), relu_in_lb)
+                relu_out_ub = torch.maximum(torch.tensor(0, dtype=data_type).to(device), relu_in_ub)
+                layer_out_bounds_per_layer.append([relu_out_lb, relu_out_ub])
+
+                affine_in_lb = relu_out_lb
+                affine_in_ub = relu_out_ub
             else:
-                next_upper_bounds = ub
-                next_lower_bounds = lb
+                layer_out_bounds_per_layer.append([relu_in_lb, relu_in_ub])
 
-            bounds_per_layer.append([next_lower_bounds, next_upper_bounds])
+        return affine_out_bounds_per_layer, layer_out_bounds_per_layer
 
-        return bounds_per_layer
-
-    def add_constraints(self, model, input_vars, bounds):
+    def add_constraints(self, model, input_vars, bounds_affine_out, bounds_layer_out):
         parameter_list = list(self.parameters())
         output_vars = model.addMVar(self.layer_widths[-1], lb=-float('inf'))  # todo bounds anpassen
 
         in_var = input_vars
+        out_vars = in_var
         for i in range(0, len(parameter_list), 2):
-            lb = bounds[int(i / 2)][0].detach().cpu().numpy()
-            ub = bounds[int(i / 2)][1].detach().cpu().numpy()
+            affine_lb = bounds_affine_out[int(i / 2)][0].detach().cpu().numpy()
+            affine_ub = bounds_affine_out[int(i / 2)][1].detach().cpu().numpy()
             W, b = parameter_list[i].detach().cpu().numpy(), parameter_list[i + 1].detach().cpu().numpy()
 
             out_fet = len(b)
-            out_vars = model.addMVar(out_fet, lb=lb, ub=ub, name="affine_var" + str(i))
+            out_vars = model.addMVar(out_fet, lb=affine_lb, ub=affine_ub, name="affine_var" + str(i))
             const = model.addConstrs((W[i] @ in_var + b[i] == out_vars[i] for i in range(len(W))))
             in_var = out_vars
 
             if i < len(parameter_list) - 2:
-                relu_vars = verbas.add_relu_constr(model, in_var, out_fet, [-10000 for i in range(len(W))], ub, i)
+                out_lb = bounds_layer_out[int(i / 2) - 1][0]
+                out_ub = bounds_layer_out[int(i / 2) - 1][1]
+                relu_vars = verbas.add_relu_constr(model, in_var, out_fet, affine_lb, affine_ub, out_lb, out_ub, i)
                 in_var = relu_vars
                 out_vars = relu_vars
 
@@ -251,7 +258,7 @@ class ICNN(nn.Module, VerifiableNet):
             last[0].data = torch.mul(torch.ones_like(last[0], dtype=data_type).to(device), self.init_scaling)
             last[1].data = torch.zeros_like(last[1], dtype=data_type).to(device)
 
-    def calculate_box_bounds(self, input_bounds, with_relu=True):  # todo für andere Netze die Architektur anpassen
+    def calculate_box_bounds(self, input_bounds):  # todo für andere Netze die Architektur anpassen
         parameter_list = list(self.parameters())
         # todo for now this only works for sequential nets
 
@@ -261,66 +268,67 @@ class ICNN(nn.Module, VerifiableNet):
                                 range(0, len(parameter_list), 2)]
             return bounds_per_layer  # todo None entfernen aus aufrufen und durch sinnvolle eingabe ersetzen
 
-        next_lower_bounds = input_bounds[0]
-        next_upper_bounds = input_bounds[1]
-        bounds_per_layer = []
+        affine_in_lb = input_bounds[0]
+        affine_in_ub = input_bounds[1]
+        affine_in_bounds_per_layer = []
+        layer_out_bounds_per_layer = []
         for i in range(0, len(parameter_list), 2):
             W, b = parameter_list[i], parameter_list[i + 1]
             w_plus = torch.maximum(W, torch.tensor(0, dtype=data_type).to(device))
             w_minus = torch.minimum(W, torch.tensor(0, dtype=data_type).to(device))
-            lb = torch.matmul(w_plus, next_lower_bounds).add(torch.matmul(w_minus, next_upper_bounds)).add(b)
-            ub = torch.matmul(w_plus, next_upper_bounds).add(torch.matmul(w_minus, next_lower_bounds)).add(b)
+            relu_in_lb = torch.matmul(w_plus, affine_in_lb).add(torch.matmul(w_minus, affine_in_ub)).add(b)
+            relu_in_ub = torch.matmul(w_plus, affine_in_ub).add(torch.matmul(w_minus, affine_in_lb)).add(b)
             if i != 0:
                 affine_u = self.us[i // 2 - 1]
                 u_plus = torch.maximum(affine_u, torch.tensor(0, dtype=data_type).to(device))
                 u_minus = torch.minimum(affine_u, torch.tensor(0, dtype=data_type).to(device))
-                lb = lb.add(torch.matmul(u_plus, next_lower_bounds).add(torch.matmul(u_minus, next_upper_bounds)))
-                ub = ub.add(torch.matmul(u_plus, next_upper_bounds).add(torch.matmul(u_minus, next_lower_bounds)))
+                relu_in_lb = relu_in_lb.add(torch.matmul(u_plus, affine_in_lb).add(torch.matmul(u_minus, affine_in_ub)))
+                relu_in_ub = relu_in_ub.add(torch.matmul(u_plus, affine_in_ub).add(torch.matmul(u_minus, affine_in_lb)))
 
-            if with_relu and i < len(parameter_list) - 2:
-                next_upper_bounds = torch.maximum(torch.tensor(0, dtype=data_type).to(device), ub)
-                next_lower_bounds = torch.maximum(torch.tensor(0, dtype=data_type).to(device), lb)
+            affine_in_bounds_per_layer.append([relu_in_lb, relu_in_ub])
+
+            if i < len(parameter_list) - 2:
+                relu_out_lb = torch.maximum(torch.tensor(0, dtype=data_type).to(device), relu_in_lb)
+                relu_out_ub = torch.maximum(torch.tensor(0, dtype=data_type).to(device), relu_in_ub)
+                layer_out_bounds_per_layer.append([relu_out_lb, relu_out_ub])
+
+                affine_in_lb = relu_out_lb
+                affine_in_ub = relu_out_ub
             else:
-                next_upper_bounds = ub
-                next_lower_bounds = lb
+                layer_out_bounds_per_layer.append([relu_in_lb, relu_in_ub])
 
-            bounds_per_layer.append([next_lower_bounds, next_upper_bounds])
+        return affine_in_bounds_per_layer, layer_out_bounds_per_layer
 
-        return bounds_per_layer
-
-    def add_constraints(self, model, input_vars, bounds):
+    def add_constraints(self, model, input_vars, bounds_affine_out, bounds_layer_out):
         ws = list(self.ws.parameters())
         us = list(self.us.parameters())
         output_vars = model.addMVar(self.layer_widths[-1], lb=-float('inf'))  # todo bounds anpassen
 
         in_var = input_vars
+        out_vars = in_var
         for i in range(0, len(ws), 2):
-            lb = bounds[int(i / 2)][0].detach().cpu().numpy()
-            ub = bounds[int(i / 2)][1].detach().cpu().numpy()
+            affine_lb = bounds_affine_out[int(i / 2)][0].detach().cpu().numpy()
+            affine_ub = bounds_affine_out[int(i / 2)][1].detach().cpu().numpy()
             affine_w, affine_b = ws[i].detach().cpu().numpy(), ws[i + 1].detach().cpu().numpy()
 
             out_fet = len(affine_b)
-            affine_var = model.addMVar(out_fet, lb=lb, ub=ub, name="affine_var" + str(i))
-            out_vars = model.addMVar(out_fet, lb=lb, ub=ub, name="affine_skip_var" + str(i))
+            affine_out_vars = model.addMVar(out_fet, lb=affine_lb, ub=affine_ub, name="affine_skip_var" + str(i))
 
-            affine_const = model.addConstrs(
-                affine_w[i] @ in_var + affine_b[i] == affine_var[i] for i in range(len(affine_w)))
             if i != 0:
                 k = math.floor(i / 2) - 1
                 skip_W = torch.clone(us[k]).detach().cpu().numpy()  # has no bias
-                skip_var = model.addMVar(out_fet, lb=lb, ub=ub, name="skip_var" + str(k))
-                skip_const = model.addConstrs(skip_W[i] @ input_vars == skip_var[i] for i in range(len(affine_w)))
-                affine_skip_cons = model.addConstrs(
-                    affine_var[i] + skip_var[i] == out_vars[i] for i in range(len(affine_w)))
+                skip_var = model.addMVar(out_fet, lb=affine_lb, ub=affine_ub, name="skip_var" + str(k))
+                skip_const = model.addConstrs(affine_w[i] @ in_var + affine_b[i] + skip_W[i] @ input_vars == affine_out_vars[i] for i in range(len(affine_w)))
             else:
-                affine_no_skip_cons = model.addConstrs(affine_var[i] == out_vars[i] for i in range(len(affine_w)))
-
-            in_var = out_vars
+                affine_no_skip_cons = model.addConstrs(affine_w[i] @ in_var + affine_b[i] == affine_out_vars[i] for i in range(len(affine_w)))
+            out_vars = affine_out_vars
+            in_var = affine_out_vars
 
             if i < len(ws) - 2:
-                relu_vars = verbas.add_relu_constr(model, in_var, out_fet, lb, ub, i)
+                out_lb = bounds_layer_out[int(i / 2) - 1][0]
+                out_ub = bounds_layer_out[int(i / 2) - 1][1]
+                relu_vars = verbas.add_relu_constr(model, in_var, out_fet, affine_lb, affine_ub, out_lb, out_ub, i)
                 in_var = relu_vars
-                out_vars = relu_vars
 
         const = model.addConstrs(out_vars[i] == output_vars[i] for i in range(out_fet))
         return output_vars
@@ -415,11 +423,11 @@ class ICNNLogical(ICNN):
             bb[0].data = u
             bb[1].data = b
 
-    def add_max_output_constraints(self, model, input_vars, bounds):
-        icnn_output_var = super().add_constraints(model, input_vars, bounds)
+    def add_max_output_constraints(self, model, input_vars, bounds_affine_out, bounds_layer_out):
+        icnn_output_var = super().add_constraints(model, input_vars, bounds_affine_out, bounds_layer_out)
         output_of_and = model.addMVar(1, lb=-float('inf'))
-        lb = bounds[-1][0].detach().cpu().numpy()
-        ub = bounds[-1][1].detach().cpu().numpy()  # todo richtige box bounds anwenden (die vom zu approximierenden Layer)
+        lb = -float("inf")
+        ub = float("inf")  # todo richtige box bounds anwenden (die vom zu approximierenden Layer)
         ls = self.ls
 
         bb_w, bb_b = ls[0].weight.data.detach().cpu().numpy(), ls[0].bias.data.detach().cpu().numpy()
@@ -539,11 +547,11 @@ class ICNNApproxMax(ICNN):
             bb[0].data = u
             bb[1].data = b
 
-    def add_max_output_constraints(self, model, input_vars, bounds):
-        icnn_output_var = super().add_constraints(model, input_vars, bounds)
+    def add_max_output_constraints(self, model, input_vars, bounds_affine_out, bounds_layer_out):
+        icnn_output_var = super().add_constraints(model, input_vars, bounds_affine_out, bounds_layer_out)
         output_of_and = model.addMVar(1, lb=-float('inf'))
-        lb = bounds[-1][0].detach().cpu().numpy()
-        ub = bounds[-1][1].detach().cpu().numpy() # todo richtige box bounds anwenden (die vom zu approximierenden Layer)
+        lb = - float("inf")
+        ub = float("inf")  # todo richtige box bounds anwenden (die vom zu approximierenden Layer)
         ls = self.ls
 
         bb_w, bb_b = ls[0].weight.data.detach().cpu().numpy(), ls[0].bias.data.detach().cpu().numpy()
@@ -615,6 +623,7 @@ def boltzmann_constraints(model, input_var, output_var, parameter):
     model.addConstr(output_var == mul_var.sum() * divide_var)
 
 
+# todo bounds für die constraints anpassen
 def mellowmax(x, parameter):
     scale = torch.mul(x, parameter)
     exp = torch.exp(scale)

script/Verification/Verification.py
@@ -11,7 +11,7 @@ def load(icnn):
     icnn.load_state_dict(torch.load("../convexHullModel.pth"), strict=False)
 
 
-def verification(icnn, from_to_neuron, center_eps_w_b=None, a_b=None, icnn_w_b_c=None, has_relu=False):
+def verification(icnn, from_to_neuron, current_layer_index, bounds_affine_out, bounds_layer_out, center_eps_w_b=None, a_b=None, icnn_w_b_c=None, has_relu=False):
     m = Model()
 
     if center_eps_w_b is not None:
@@ -22,24 +22,21 @@ def verification(icnn, from_to_neuron, center_eps_w_b=None, a_b=None, icnn_w_b_c
         input_size = len(b)
 
         input_to_previous_layer_size = affine_w.shape[1]
-        input_to_previous_layer = m.addMVar(input_to_previous_layer_size, lb=-float('inf'))
+        input_to_previous_layer = m.addMVar(input_to_previous_layer_size, lb=[elem - eps for elem in center],
+                              ub=[elem + eps for elem in center])
 
-        lb = [-10000 for i in range(input_to_previous_layer_size)]
-        ub = [10000 for i in range(input_to_previous_layer_size)]  # todo mit boxbounds anpassen
+        m.addConstrs(input_to_previous_layer[i] <= center[i] + eps for i in range(input_to_previous_layer_size))
+        m.addConstrs(input_to_previous_layer[i] >= center[i] - eps for i in range(input_to_previous_layer_size))
 
-        max_vars = m.addVars(input_to_previous_layer_size, lb=-float('inf'))
-        min_vars = m.addVars(input_to_previous_layer_size, lb=-float('inf'))
+        in_lb = bounds_affine_out[current_layer_index - 1][0]
+        in_ub = bounds_affine_out[current_layer_index - 1][1]
+        out_lb = bounds_layer_out[current_layer_index - 1][0]
+        out_ub = bounds_layer_out[current_layer_index - 1][1]
 
-        m.addConstrs(max_vars[i] == center[i] + eps for i in range(input_to_previous_layer_size))
-        m.addConstrs(min_vars[i] == center[i] - eps for i in range(input_to_previous_layer_size))
-
-        m.addConstrs(input_to_previous_layer[i] <= max_vars[i] for i in range(input_to_previous_layer_size))
-        m.addConstrs(input_to_previous_layer[i] >= min_vars[i] for i in range(input_to_previous_layer_size))
-
-        affine_out = add_affine_constr(m, affine_w, b, input_to_previous_layer, lb, ub)
+        affine_out = add_affine_constr(m, affine_w, b, input_to_previous_layer, in_lb, in_ub)
 
         if has_relu:
-            relu_out = add_relu_constr(m, affine_out, input_size, lb, ub)
+            relu_out = add_relu_constr(m, affine_out, input_size, in_lb, in_ub, out_lb, out_ub)
             input_var = relu_out
         else:
             input_var = affine_out
@@ -48,7 +45,7 @@ def verification(icnn, from_to_neuron, center_eps_w_b=None, a_b=None, icnn_w_b_c
         A = a_b[0]
         b = a_b[1]
         input_size = len(b)
-        input_var = m.addMVar(input_size, lb=-float('inf'), name="in_var")
+        input_var = m.addMVar(input_size, lb=-float('inf'), name="in_var") # todo mit boxbounds anpassen
         m.addMConstr(A, input_var, "<=", b)
 
     elif icnn_w_b_c is not None:
@@ -64,8 +61,8 @@ def verification(icnn, from_to_neuron, center_eps_w_b=None, a_b=None, icnn_w_b_c
         input_to_previous_layer = m.addMVar(constraint_icnn_input_size, lb=-float('inf'))
 
         for k in range(len(constraint_icnn)):
-            bounds = constraint_icnn[k].calculate_box_bounds(None)
-            constraint_icnn[k].add_max_output_constraints(m, input_to_previous_layer[from_to_neuron[0]: from_to_neuron[1]], bounds)
+            constraint_icnn_bounds_affine_out, constraint_icnn_bounds_layer_out = constraint_icnn[k].calculate_box_bounds(bounds_layer_out[current_layer_index - 1])
+            constraint_icnn[k].add_max_output_constraints(m, input_to_previous_layer[from_to_neuron[0]: from_to_neuron[1]], constraint_icnn_bounds_affine_out, constraint_icnn_bounds_layer_out)
 
         if has_relu:
             relu_var = m.addMVar(input_size, lb=-float('inf'), name="in_var")
@@ -80,8 +77,8 @@ def verification(icnn, from_to_neuron, center_eps_w_b=None, a_b=None, icnn_w_b_c
     # todo das kann man optimieren in dem man die constraints, nur für die relevanten neuronen erstellt und nicht für alle
     input_var = input_var[from_to_neuron[0]: from_to_neuron[1]]
 
-    bounds = icnn.calculate_box_bounds(None)
-    output_var = icnn.add_constraints(m, input_var, bounds)
+    icnn_bounds_affine_out, icnn_bounds_layer_out = icnn.calculate_box_bounds(bounds_layer_out[current_layer_index])
+    output_var = icnn.add_constraints(m, input_var, icnn_bounds_affine_out, icnn_bounds_layer_out)
 
     m.update()
     m.setObjective(output_var[0], GRB.MAXIMIZE)

script/Verification/Verifier.py
@@ -97,36 +97,39 @@ class SingleNeuronVerifier(Verifier):
 
         input_flattened = torch.flatten(self.input_x)
         input_size = input_flattened.size(0)
-        bounds = self.net.calculate_box_bounds([input_flattened.add(-self.eps), input_flattened.add(self.eps)], with_relu=True)
+        bounds_affine_out, bounds_layer_out = self.net.calculate_box_bounds([input_flattened.add(-self.eps), input_flattened.add(self.eps)])
 
         input_flattened = input_flattened.cpu().numpy()
 
-        input_var = m.addMVar(input_size, lb=-float("inf"), name="in_var")
-        m.addConstrs((input_var[i] <= input_flattened[i] + self.eps for i in range(input_size)), name="in_const1")
+        input_var = m.addMVar(input_size, lb=[elem - self.eps for elem in input_flattened],
+                              ub=[elem + self.eps for elem in input_flattened], name="in_var")
+        m.addConstrs((input_var[i] <= input_flattened[i] + self.eps for i in range(input_size)), name="in_const0")
         m.addConstrs((input_var[i] >= input_flattened[i] - self.eps for i in range(input_size)), name="in_const1")
 
 
         parameter_list = list(self.net.parameters())
         in_var = input_var
         for i in range(0, len(parameter_list) - 2, 2):
-            lb = bounds[int(i / 2)][0].detach().cpu().numpy()
-            ub = bounds[int(i / 2)][1].detach().cpu().numpy()
+            in_lb = bounds_affine_out[int(i / 2)][0].detach().cpu().numpy()
+            in_ub = bounds_affine_out[int(i / 2)][1].detach().cpu().numpy()
             W, b = parameter_list[i].detach().cpu().numpy(), parameter_list[i + 1].detach().cpu().numpy()
 
             out_fet = len(b)
-            out_vars = m.addMVar(out_fet, lb=-float("inf"), name="affine_var" + str(i))
+            out_vars = m.addMVar(out_fet, lb=in_lb, ub=in_ub, name="affine_var" + str(i))
             const = m.addConstrs((W[i] @ in_var + b[i] == out_vars[i] for i in range(len(W))), name="affine_const" + str(i))
 
             relu_in_var = out_vars
-            relu_vars = verbas.add_singel_neuron_constr(m, relu_in_var, out_fet, lb, ub, i=i)  # verbas.add_relu_constr(m, relu_in_var, out_fet, lb, ub)
+            out_lb = bounds_layer_out[int(i / 2)][0].detach().cpu().numpy()
+            out_ub = bounds_layer_out[int(i / 2)][1].detach().cpu().numpy()
+            relu_vars = verbas.add_single_neuron_constr(m, relu_in_var, out_fet, in_lb, in_ub, out_lb, out_ub, i=i)
             in_var = relu_vars
 
-        lb = bounds[-1][0].detach().cpu().numpy()
-        ub = bounds[-1][1].detach().cpu().numpy()
+        lb = bounds_affine_out[-1][0].detach().cpu().numpy()
+        ub = bounds_affine_out[-1][1].detach().cpu().numpy()
         W, b = parameter_list[len(parameter_list) - 2].detach().cpu().numpy(), parameter_list[-1].detach().cpu().numpy()
 
         out_fet = len(b)
-        out_vars = m.addMVar(out_fet, lb=-float("inf"), name="last_affine_var")
+        out_vars = m.addMVar(out_fet, lb=lb, ub=ub, name="last_affine_var")
         const = m.addConstrs((W[i] @ in_var + b[i] == out_vars[i] for i in range(len(W))), name="out_const")
         m.update()
         self.model = m
@@ -154,35 +157,39 @@ class MILPVerifier(Verifier):
         input_size = input_flattened.size(0)
         # todo hier darf with_relu nicht wahr sein, weil man sonst ggf bestimmte abhängigkeiten
         #  der Neruonen unterbindet, aber möglicherweise kann ich das bei SNV oder DHOV verwenden?
-        bounds = self.net.calculate_box_bounds([input_flattened.add(-self.eps), input_flattened.add(self.eps)], with_relu=True)
+        bounds_affine_out, bounds_layer_out = self.net.calculate_box_bounds([input_flattened.add(-self.eps), input_flattened.add(self.eps)])
 
-        input_flattened = input_flattened.numpy()
+        input_flattened = input_flattened.cpu().numpy()
 
-        input_var = m.addMVar(input_size, lb=-float("inf"), name="in_var")
+        input_var = m.addMVar(input_size, lb=[elem - self.eps for elem in input_flattened],
+                              ub=[elem + self.eps for elem in input_flattened], name="in_var")
         m.addConstrs(input_var[i] <= input_flattened[i] + self.eps for i in range(input_size))
         m.addConstrs(input_var[i] >= input_flattened[i] - self.eps for i in range(input_size))
+
 
         parameter_list = list(self.net.parameters())
         in_var = input_var
         for i in range(0, len(parameter_list) - 2, 2):
-            lb = bounds[int(i / 2)][0].detach().numpy()
-            ub = bounds[int(i / 2)][1].detach().numpy()
+            in_lb = bounds_affine_out[int(i / 2)][0].detach().numpy()
+            in_ub = bounds_affine_out[int(i / 2)][1].detach().numpy()
             W, b = parameter_list[i].detach().numpy(), parameter_list[i + 1].detach().numpy()
 
             out_fet = len(b)
-            out_vars = m.addMVar(out_fet, lb=-float("inf"), name="affine_var" + str(i))
+            out_vars = m.addMVar(out_fet, lb=in_lb, ub=in_ub, name="affine_var" + str(i))
             const = m.addConstrs((W[i] @ in_var + b[i] == out_vars[i] for i in range(len(W))))
 
             relu_in_var = out_vars
-            relu_vars = verbas.add_relu_constr(m, relu_in_var, out_fet, lb, ub)
+            out_lb = bounds_layer_out[int(i / 2)][0].detach().cpu().numpy()
+            out_ub = bounds_layer_out[int(i / 2)][1].detach().cpu().numpy()
+            relu_vars = verbas.add_relu_constr(m, relu_in_var, out_fet, in_lb, in_ub, out_lb, out_ub)
             in_var = relu_vars
 
-        lb = bounds[-1][0].detach().numpy()
-        ub = bounds[-1][1].detach().numpy()
+        lb = bounds_affine_out[-1][0].detach().numpy()
+        ub = bounds_affine_out[-1][1].detach().numpy()
         W, b = parameter_list[len(parameter_list) - 2].detach().numpy(), parameter_list[-1].detach().numpy()
 
         out_fet = len(b)
-        out_vars = m.addMVar(out_fet, lb=-float("inf"), name="last_affine_var")
+        out_vars = m.addMVar(out_fet, lb=lb, ub=ub, name="last_affine_var")
         const = m.addConstrs((W[i] @ in_var + b[i] == out_vars[i] for i in range(len(W))), name="out_const")
         m.update()
         self.model = m
@@ -211,20 +218,21 @@ class DHOVVerifier(Verifier):
 
         input_flattened = torch.flatten(self.input_x)
         input_size = input_flattened.size(0)
-        bounds = self.net.calculate_box_bounds([input_flattened.add(-self.eps), input_flattened.add(self.eps)], with_relu=True)
+        bounds_affine_out, bounds_layer_out = self.net.calculate_box_bounds([input_flattened.add(-self.eps), input_flattened.add(self.eps)])
 
         input_flattened = input_flattened.cpu().numpy()
-
-        input_var = m.addMVar(input_size, lb=[elem - self.eps for elem in input_flattened],
-                              ub=[elem + self.eps for elem in input_flattened], name="in_var")
+        in_var_bounds = [[elem - self.eps for elem in input_flattened], [elem + self.eps for elem in input_flattened]]
+        input_var = m.addMVar(input_size, lb=in_var_bounds[0],
+                              ub=in_var_bounds[1], name="in_var")
         m.addConstrs(input_var[i] <= input_flattened[i] + self.eps for i in range(input_size))
         m.addConstrs(input_var[i] >= input_flattened[i] - self.eps for i in range(input_size))
 
         parameter_list = list(self.net.parameters())
         in_var = input_var
+
         for i in range(0, len(parameter_list) - 2, 2):
-            lb = bounds[int(i / 2)][0].detach().cpu().numpy()
-            ub = bounds[int(i / 2)][1].detach().cpu().numpy()
+            lb = bounds_affine_out[int(i / 2)][0].detach().cpu().numpy()
+            ub = bounds_affine_out[int(i / 2)][1].detach().cpu().numpy()
             W, b = parameter_list[i].detach().cpu().numpy(), parameter_list[i + 1].detach().cpu().numpy()
 
             out_fet = len(b)
@@ -242,13 +250,15 @@ class DHOVVerifier(Verifier):
                         from_neuron = self.group_size * group_i
                         to_neuron = self.group_size * group_i + self.group_size  # upper bound is exclusive
 
-                    self.icnns[i // 2][group_i].add_max_output_constraints(m, in_var[from_neuron:to_neuron], self.icnns[i // 2][group_i].calculate_box_bounds(None))
+                    constraint_bounds_affine_out, constraint_bounds_layer_out = self.icnns[i // 2][group_i].calculate_box_bounds(in_var_bounds)
+                    self.icnns[i // 2][group_i].add_max_output_constraints(m, in_var[from_neuron:to_neuron], constraint_bounds_affine_out, constraint_bounds_layer_out)
 
             #m.update()
             in_var = out_vars
+            in_var_bounds = [lb, ub]
 
-        lb = bounds[-1][0].detach().cpu().numpy()
-        ub = bounds[-1][1].detach().cpu().numpy()
+        lb = bounds_affine_out[-1][0].detach().cpu().numpy()
+        ub = bounds_affine_out[-1][1].detach().cpu().numpy()
         W, b = parameter_list[len(parameter_list) - 2].detach().cpu().numpy(), parameter_list[-1].detach().cpu().numpy()
 
         out_fet = len(b)