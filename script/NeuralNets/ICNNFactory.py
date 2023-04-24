05878cd771e35e83c930408d0ec9099e0040a61b
ufukguenes <u.guenes@live.de>
2023-04-24 03:33:12 +0200

    allowing to use no init in ICNN

--- script/NeuralNets/ICNNFactory.py
+++ script/NeuralNets/ICNNFactory.py
@@ -2,11 +2,12 @@ from script.NeuralNets.Networks import ICNN, ICNNLogical, ICNNApproxMax
 
 
 class ICNNFactory():
-    def __init__(self, icnn_type, layer_width, *args, **kwargs):
+    def __init__(self, icnn_type, layer_width, adapt_layer_for_init=False, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
         self.icnn_type = icnn_type
         self.layer_width = layer_width
+        self.adapt_layer_for_init = adapt_layer_for_init
         valid_icnn_types = ["standard", "logical", "approx_max"]
         if icnn_type not in valid_icnn_types:
             raise AttributeError("Expected plotting mode one of: {}, actual: {}".format(valid_icnn_types, icnn_type))
@@ -15,7 +16,8 @@ class ICNNFactory():
         new_layer_width = self.layer_width.copy()
         new_layer_width.insert(0, in_layer_size_as_group_size)
         if self.icnn_type == "standard":
-            new_layer_width.insert(len(new_layer_width) - 1, 2 * in_layer_size_as_group_size)
+            if self.adapt_layer_for_init:
+                new_layer_width.insert(len(new_layer_width) - 1, 2 * in_layer_size_as_group_size)
             return ICNN(new_layer_width, *self.args, **self.kwargs)
         elif self.icnn_type == "logical":
             return ICNNLogical(new_layer_width, *self.args, **self.kwargs)
