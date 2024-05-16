d0d77db747d3cc287576d72a191e3dadb7e80fed
ufukguenes <u.guenes@live.de>
2024-05-16 12:44:08 +0200

    add profiling capabilities

---
+++ script/Profiling.py
@@ -0,0 +1,12 @@
+
+class Timings:
+    def __init__(self):
+        self.icnn_verification_time_per_layer_per_icnn = []
+        self.icnn_verification_time_per_layer_total = []
+
+        self.icnn_training_time_per_layer_per_icnn = []
+        self.icnn_training_time_per_layer_total = []
+
+        self.neuron_refinement_time_per_layer = []
+        self.data_sampling_time_per_layer = []
+        self.total_time_per_layer = []
